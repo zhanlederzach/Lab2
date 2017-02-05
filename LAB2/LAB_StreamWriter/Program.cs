56ee40d25987f69e8693bcf9aa99abed4defa79c
zhanelderzach <miras_13@bk.ru>
2017-02-05 20:21:33 +0600

    StreamWriter

---
+++ LAB2/LAB_StreamWriter/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_StreamWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            FileStream fs2 = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs2);
+            StreamReader sr = new StreamReader(fs);
+
+            string str = sr.ReadToEnd();
+
+            sr.Close();
+            fs.Close();
+
+            string[] input = str.Split(' ');
+
+            int result = -4;
+
+            for (int i = 1; i<input.Length;++i)
+            {
+                int current = int.Parse(input[i]);
+                bool isPrime = true;
+                if (current != 2)
+                {
+                    for (int j = 2; j <= Math.Sqrt(current); j++)
+                    {
+                        if (current % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (isPrime == true && result == -4) {
+                    result = current;
+                } else
+                {
+                    if (isPrime == true) {
+                        if (current < result)
+                            result = current;
+                    }
+                }
+
+            }
+
+            sw.WriteLine(result);
+            sw.Close();
+
+        }
+       
+    }
+}
---
+++ LAB2/StreamReader/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_StreamReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // открываем или создаем (если нет) тхт файл для чтения
+            FileStream fs = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            
+            // читаем его
+            StreamReader sr = new StreamReader(fs);
+
+            // читаем от начала до конца
+            string inputLine = sr.ReadToEnd();
+
+            // закрываем поток
+            sr.Close();
+            fs.Close();
+            // разграничиваем пробелом нам массив
+            string[] inputs = inputLine.Split(' ');
+
+            // находим минимум и максимум 
+            int min = int.Parse(inputs[0]);
+
+            int max = int.Parse(inputs[0]); 
+
+            for (int i=0; i<inputs.Length; ++i)
+            {
+                int curent = int.Parse(inputs[i]);
+
+                if (curent > max)
+                {
+                    max = curent;
+                }
+
+                if (curent < min)
+                {
+                    min = curent;
+                }                  
+            }
+            // выводим на экран
+            Console.WriteLine("min={0}, max={1}", min, max);
+            Console.ReadKey();    
+        }
+
+    }
+}
