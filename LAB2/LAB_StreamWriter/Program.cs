abeca1d97e95ed6311a9d19dec3c408c84284221
zhanelderzach <miras_13@bk.ru>
2017-02-05 20:52:31 +0600

    added some comments

--- LAB2/LAB_StreamWriter/Program.cs
+++ LAB2/LAB_StreamWriter/Program.cs
@@ -11,22 +11,27 @@ namespace LAB_StreamWriter
     {
         static void Main(string[] args)
         {
+            // создаем стрим для записи
             FileStream fs = new FileStream("input.txt", FileMode.OpenOrCreate, FileAccess.Read);
+            // создаем стрим для чтения
             FileStream fs2 = new FileStream("output.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            
             StreamWriter sw = new StreamWriter(fs2);
             StreamReader sr = new StreamReader(fs);
-
+            // читаем нам файл от начала до конца
             string str = sr.ReadToEnd();
-
+            // закрываем потоки чтения
             sr.Close();
             fs.Close();
-
+            // разделяем содержимое файла пробелами
             string[] input = str.Split(' ');
 
+            //алгоритм нахождения наименьшего простого числа
             int result = -4;
 
             for (int i = 1; i<input.Length;++i)
             {
+                //конвертируем его в целые числа
                 int current = int.Parse(input[i]);
                 bool isPrime = true;
                 if (current != 2)
@@ -52,8 +57,9 @@ namespace LAB_StreamWriter
                 }
 
             }
-
+            // запимываем результат в новый файл для записи
             sw.WriteLine(result);
+            // закрываем наш файл (записи)
             sw.Close();
 
         }
